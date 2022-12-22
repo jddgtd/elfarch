45d50562e9d4eb45394a2940dbea3bcda004e7ec
ali3alo <120683286+ali3alo@users.noreply.github.com>
2022-12-22 18:11:05 +0300

    Update sedthon.py

--- sedthon.py
+++ sedthon.py
@@ -789,7 +789,7 @@ ownerhson_id = 717273945
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري حبيبي - @G_5_G')
+        order = await event.reply('اهلا مطوري فراغ - @G_5_G')
 
 
 @sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.ورود"))
